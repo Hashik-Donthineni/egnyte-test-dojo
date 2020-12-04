a328543c98ca7bb0b0c985d9a7a93da8411b09a5
Hashik Donthineni <Hashikdonthineni@gmail.com>
2020-12-05 00:28:28 +0530

    Added header and layout in the middle

--- src/pages/drawer.js
+++ src/pages/drawer.js
@@ -2,11 +2,28 @@ import React from "react";
 import Drawer from "../components/Drawer";
 import { Layout } from "antd";
 
+const { Header, Content, Footer } = Layout;
+
 const DrawerPage = () => {
   return (
     <>
       <Layout style={{ minHeight: "100vh" }}>
         <Drawer />
+        <Layout>
+          <Header
+            className="site-layout-sub-header-background"
+            style={{ padding: 0, height: "44px" }}
+          />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            ></div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            DOJO Developer Guide @ Created by DOJO Team
+          </Footer>
+        </Layout>
       </Layout>
     </>
   );
