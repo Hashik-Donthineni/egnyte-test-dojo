3edf755d3f923d8131db7b4bc001c69208a5b507
Hashik Donthineni <Hashikdonthineni@gmail.com>
2020-12-05 00:40:29 +0530

    Changed DrawerPage to classcomponent

--- src/pages/drawer.js
+++ src/pages/drawer.js
@@ -4,35 +4,37 @@ import { Layout } from "antd";
 
 const { Header, Content, Footer } = Layout;
 
-const DrawerPage = () => {
-  return (
-    <>
-      <Layout style={{ minHeight: "100vh" }}>
-        <Drawer />
-        <Layout>
-          <Header
-            className="site-layout-sub-header-background"
-            style={{
-              padding: 0,
-              height: "44px",
-              background: "white",
-            }}
-          />
-          <Content style={{ margin: "24px 16px 0" }}>
-            <div
-              className="site-layout-background"
-              style={{ padding: 24, minHeight: 360 }}
-            >
-              INJECT A COMPONENT HERE
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            DOJO Developer Guide @ Created by DOJO Team
-          </Footer>
+class DrawerPage extends React.Component {
+  render() {
+    return (
+      <>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Drawer />
+          <Layout>
+            <Header
+              className="site-layout-sub-header-background"
+              style={{
+                padding: 0,
+                height: "44px",
+                background: "white",
+              }}
+            />
+            <Content style={{ margin: "24px 16px 0" }}>
+              <div
+                className="site-layout-background"
+                style={{ padding: 24, minHeight: 360 }}
+              >
+                INJECT A COMPONENT HERE
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              DOJO Developer Guide @ Created by DOJO Team
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default DrawerPage;
