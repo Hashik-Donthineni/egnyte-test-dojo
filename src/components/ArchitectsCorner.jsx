dbed76cfa14c4893cd507137fbf05ce8830faa94
Hashik Donthineni <Hashikdonthineni@gmail.com>
2020-12-09 13:38:57 +0530

    Added a list of blogs and rendered them

--- src/components/ArchitectsCorner.jsx
+++ src/components/ArchitectsCorner.jsx
@@ -4,8 +4,79 @@ import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
 
 class ArchitectsCorner extends React.Component {
   state = {};
+
+  IconText = ({ icon, text }) => (
+    <Space>
+      {React.createElement(icon)}
+      {text}
+    </Space>
+  );
+
   render() {
-    return "Hello";
+    const listData = [];
+
+    for (let i = 0; i < 20; i++) {
+      listData.push({
+        href: "https://ant.design",
+        title: `ant design part ${i}`,
+        avatar:
+          "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        description:
+          "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+        content:
+          "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+      });
+    }
+
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={listData}
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <this.IconText
+                icon={StarOutlined}
+                text="156"
+                key="list-vertical-star-o"
+              />,
+              <this.IconText
+                icon={LikeOutlined}
+                text="156"
+                key="list-vertical-like-o"
+              />,
+              <this.IconText
+                icon={MessageOutlined}
+                text="2"
+                key="list-vertical-message"
+              />,
+            ]}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            }
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={item.avatar} />}
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+            {item.content}
+          </List.Item>
+        )}
+      />
+    );
   }
 }
 
