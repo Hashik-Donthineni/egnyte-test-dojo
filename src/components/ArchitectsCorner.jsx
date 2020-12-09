dd5bc8d693cf84339892bf594a52eb6a507d4203
Hashik Donthineni <Hashikdonthineni@gmail.com>
2020-12-09 14:47:19 +0530

    Moved blog data to static folder

--- src/components/ArchitectsCorner.jsx
+++ src/components/ArchitectsCorner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, List, Avatar, Space } from "antd";
 import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
+import { listData } from "../../static/blogdata.js";
 
 class ArchitectsCorner extends React.Component {
   state = {};
@@ -13,105 +14,6 @@ class ArchitectsCorner extends React.Component {
   );
 
   render() {
-    const listData = [
-      {
-        href:
-          "https://www.codeproject.com/Articles/5289290/Routing-Orthogonal-Diagram-Connectors-in-JavaScrip",
-        title: `Routing Orthogonal Diagram Connectors in JavaScript`,
-        avatar:
-          "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-        description:
-          "An orthogonal connector uses only vertical and horizontal lines to connect two shapes",
-        content:
-          "Everything was going all right until we hit the point of connecting the shapes. We were shooting of course for orthogonal connectors as the best way of displaying beautiful diagrams. It was one of those things you just don’t think much about, how it works or how complex the implementation could be; but turned out to be a way more sophisticated task than anticipated.",
-        image:
-          "https://www.visme.co/wp-content/uploads/2020/01/Flowchart-Expand-Page-05.png",
-        like: "155",
-        comment: "72",
-        star: "12",
-      },
-      {
-        href:
-          "https://www.codeproject.com/Tips/5270917/WebView2-for-Folder-Based-Browsing-of-Animated-GIF",
-        title: `Routing Orthogonal Diagram Connectors in JavaScript`,
-        avatar:
-          "https://image.freepik.com/free-vector/strong-asian-woman-profile-vector_53876-61424.jpg",
-        description:
-          "First exploration of the capabilities of Chromium WebView2 Control to display",
-        content:
-          "The status of the WebView2 Control has changed from preview to Generally Available (GA). The Microsoft Edge WebView2 control enables you to embed web technologies (HTML... ) in native applications. It uses Microsoft Edge (Chromium) as the rendering engine to display web content .... See here for the pitch for a hybrid native/web app approach",
-        image:
-          "https://img.freepik.com/free-vector/process-chart-with-five-elements-template_1262-7978.jpg?size=626&ext=jpg",
-        like: "100",
-        comment: "50",
-        star: "14",
-      },
-      {
-        href:
-          "https://www.codeproject.com/Tips/5270917/WebView2-for-Folder-Based-Browsing-of-Animated-GIF",
-        title: `Web Presentation, an Application in a Single File, now with Video`,
-        avatar:
-          "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-        description:
-          "A cross-platform replacement for all those office presentation applications in a single file",
-        content:
-          "Who needs presentations created with boring bulky office presentation packages not always available for all systems? All you need is a Web browser and a set of vector/pixel images. With a solution in just one HTML/CSS/JavaScript file, you have all the features people use in presentations. Since v.2, in addition to graphics and animated graphics, video elements can be added and controlled from the presentation frames.",
-        image:
-          "https://www.visme.co/wp-content/uploads/2020/01/Flowchart-Expand-Page-05.png",
-        like: "76",
-        comment: "55",
-        star: "22",
-      },
-      {
-        href:
-          "https://www.codeproject.com/Tips/5270917/WebView2-for-Folder-Based-Browsing-of-Animated-GIF",
-        title: `Routing Orthogonal Diagram Connectors in JavaScript`,
-        avatar:
-          "https://image.freepik.com/free-vector/strong-asian-woman-profile-vector_53876-61424.jpg",
-        description:
-          "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-        content:
-          "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
-        image:
-          "https://img.freepik.com/free-vector/process-chart-with-five-elements-template_1262-7978.jpg?size=626&ext=jpg",
-        like: "155",
-        comment: "72",
-        star: "12",
-      },
-      {
-        href:
-          "https://www.codeproject.com/Articles/5289290/Routing-Orthogonal-Diagram-Connectors-in-JavaScrip",
-        title: `Routing Orthogonal Diagram Connectors in JavaScript`,
-        avatar:
-          "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-        description:
-          "An orthogonal connector uses only vertical and horizontal lines to connect two shapes",
-        content:
-          "Everything was going all right until we hit the point of connecting the shapes. We were shooting of course for orthogonal connectors as the best way of displaying beautiful diagrams. It was one of those things you just don’t think much about, how it works or how complex the implementation could be; but turned out to be a way more sophisticated task than anticipated.",
-        image:
-          "https://www.visme.co/wp-content/uploads/2020/01/Flowchart-Expand-Page-05.png",
-        like: "100",
-        comment: "50",
-        star: "14",
-      },
-      {
-        href:
-          "https://www.codeproject.com/Tips/5270917/WebView2-for-Folder-Based-Browsing-of-Animated-GIF",
-        title: `Routing Orthogonal Diagram Connectors in JavaScript`,
-        avatar:
-          "https://image.freepik.com/free-vector/strong-asian-woman-profile-vector_53876-61424.jpg",
-        description:
-          "First exploration of the capabilities of Chromium WebView2 Control to display animated",
-        content:
-          "The status of the WebView2 Control has changed from preview to Generally Available (GA). The Microsoft Edge WebView2 control enables you to embed web technologies (HTML... ) in native applications. It uses Microsoft Edge (Chromium) as the rendering engine to display web content .... See here for the pitch for a hybrid native/web app approach",
-        image:
-          "https://img.freepik.com/free-vector/process-chart-with-five-elements-template_1262-7978.jpg?size=626&ext=jpg",
-        like: "76",
-        comment: "55",
-        star: "22",
-      },
-    ];
-
     return (
       <List
         itemLayout="vertical"
