10f0d6e5fa213ccd8741b4117b138d33d5d89c15
Hashik Donthineni <Hashikdonthineni@gmail.com>
2020-12-09 13:31:01 +0530

    Added architects corner

---
+++ src/components/ArchitectsCorner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { List, Avatar, Space } from "antd";
+import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
+
+class ArchitectsCorner extends React.Component {
+  state = {};
+  render() {
+    return "Hello";
+  }
+}
+
+export default ArchitectsCorner;
