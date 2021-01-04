100cc70f00de8fdcdc6be1a98dcff3fb7cedbdda
Hashik Donthineni <Hashikdonthineni@gmail.com>
2021-01-04 21:53:31 +0530

    Added the egnyte page and downloaded SDK

---
+++ src/pages/egnyte.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+class Egnyte extends React.Component {
+  state = {};
+  render() {
+    return <>Hello</>;
+  }
+}
+
+export default Egnyte;
