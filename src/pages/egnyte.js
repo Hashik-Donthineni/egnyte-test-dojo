b242fbf9ea55c511ce382ed6224dfd6944b1211f
Hashik Donthineni <Hashikdonthineni@gmail.com>
2021-01-04 22:14:21 +0530

    Created Injection in egnyte.js

--- src/pages/egnyte.js
+++ src/pages/egnyte.js
@@ -1,10 +1,34 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
+import { Button, Alert } from "antd";
+
+const injectComponent = (component) => {
+  console.log("INJECTING");
+  ReactDOM.render(component, document.getElementById("component-container"));
+};
 
 class Egnyte extends React.Component {
   state = {};
+  buttonClicked = () => {
+    injectComponent(<Alert message="Success Tips" type="success" showIcon />);
+  };
+
   render() {
-    return <>Hello</>;
+    return (
+      <>
+        <Button onClick={this.buttonClicked} type="primary">
+          Fetch Something
+        </Button>
+        <div
+          id="component-container"
+          style={{
+            padding: "0px",
+            minHeight: 360,
+            margin: "0px",
+          }}
+        />
+      </>
+    );
   }
 }
 
