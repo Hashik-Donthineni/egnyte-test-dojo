7422c298b0dba22451637c367ede19df96524391
Hashik Donthineni <Hashikdonthineni@gmail.com>
2021-01-04 23:44:41 +0530

    Fixed buld issue with Gatsby Window

--- src/pages/token.js
+++ src/pages/token.js
@@ -3,7 +3,8 @@ import React from "react";
 class AccessToken extends React.Component {
   state = {};
   render() {
-    return <>{window.location.pathname}</>;
+    // return <>{window.location.pathname}</>;
+    return <>"TOKEN"</>;
   }
 }
 
